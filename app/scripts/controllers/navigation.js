e9777271b0342f7db021265d562d9bffc50e3673
jesstedder <jesstedder@gmail.com>
2015-08-10 14:23:00 -0400

    asfdlj

--- app/scripts/controllers/navigation.js
+++ app/scripts/controllers/navigation.js
@@ -11,5 +11,9 @@ angular.module('reWebApp')
   .controller('NavigationCtrl', ["$scope", "$location", function ($scope, $location) {
     $scope.isActive= function(p){
         return (p === $location.path());
-    }
+    };
+    $scope.isCollapsed = true;
+    $scope.$on('$routeChangeSuccess', function () {
+        $scope.isCollapsed = true;
+    });    
   }]);
